0cec945be2986722be08fb4802376cecb9a068fb
saikun1 <saikun723@gmail.com>
2023-03-18 11:56:59 +0700

    Update tugas Ke-8

---
+++ pages/javascrip_dasar/javascrip/latihan.js
@@ -0,0 +1,38 @@
+let i = 0;
+
+
+while (i < 5) {
+    console.log(i);
+    i++
+}
+
+for (let i = 0; i < 5; i++) {
+    if  (i = 3){
+        continue;
+    }
+
+}
+
+function salam(){
+    function salam(){
+        //local scope/ block scope
+        //function yang tidak mengembalikan nilai  disebut void function
+
+
+        alert('selamat datang');
+        
+    }
+
+    salam();
+
+    console.log(salam());
+}
+
+function hitungluas(){
+    //return function
+
+    let panjang = 10;
+    let lebar = 5;
+    
+    let  
+}
